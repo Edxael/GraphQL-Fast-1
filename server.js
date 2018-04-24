d4b09b146320df6dc4b525bf058ebc272f920273
Edxael <zerphyr.r@gmail.com>
2018-04-24 00:06:16 -0600

    update code

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 import express from 'express'
+import { graphiqlExpress, graphqlExpress } from 'apollo-server-express'
+import bodyParser from 'body-parser'
 const app = express()
 
-console.log("Miriam...s")
+ 
 
-
-app.get('/graphql', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.use('/play', graphiqlExpress({
+    endpointURL: '/myGraphql'
+}))
+
+app.use('/myGraphql', bodyParser.json(), graphqlExpress({ }))
 
 
-app.listen(4000, (err) => {
+
+app.listen(3000, (err) => {
     if(err){ throw err }
-    console.log("\n------------------- \n Server Listening on port 4000 ... \n")
+    console.log("\n------------------- \n Server Listening on port 3000 ... \n")
 })
